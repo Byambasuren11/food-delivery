a82f8bb0cbc6d628cb08f48dee551346828e174f
Byambasuren11 <byumbasurenbold@gmail.com>
2025-02-19 17:37:58 +0800

    first commit

---
+++ src/components/Sign-Up.tsx
@@ -0,0 +1,14 @@
+const CreateAccount=()=>{
+
+    return(
+        <>
+        <div className="2/5">
+
+        </div>
+        <div className="w-3/5">
+           <img src="./bicycle.png"/>
+        </div>
+        </>
+    )
+}
+export default CreateAccount
